1351b3779d727d1a27c642cb2b2c46d87321e1e7
Sultan Ahmad Khan <saksaikot@gmail.com>
2022-02-17 21:46:15 +0600

    003 product images

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,12 +1,31 @@
+import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
+import { imageBlur } from "../lib/image";
 
-export default function ProductCard({ product: { id, title, price, url } }) {
+export default function ProductCard({
+  product: { id, title, price, url, imageProps },
+}) {
+  // console.log(id);
   return (
-    <li className="border shadow hover:shadow-xl mx-auto w-10/12">
+    <li className="border shadow hover:shadow-xl mx-auto ">
       <Link href={`/products/${id}`}>
         <a>
-          <img className="w-full" src={url} alt="" />
+          <Image
+            className="w-80"
+            src={url}
+            alt=""
+            width={320}
+            height={240}
+            quality={90}
+            {...imageProps}
+            // placeholder="blur"
+            // blurDataURL={base64}
+            // priority={id < 7 ? true : false}
+            // layout="responsive"
+            // placeholder="blur"
+            layout="fixed"
+          />
           <header className="p-2 flex justify-between items-baseline">
             <h2 className="text-lg font-semibold">{title}</h2>
             <span>{price}</span>
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,20 +2,24 @@ import Head from "next/head";
 import Link from "next/link";
 import ProductCard from "../components/ProductCard";
 import Title from "../components/Title";
+import { addImageOptimization } from "../lib/image";
 import { getProducts } from "../lib/product";
 
 export async function getStaticProps() {
+  const LCP_AMOUNT = 6;
   const products = await getProducts();
+  const optProducts = await addImageOptimization(products, LCP_AMOUNT);
+
   return {
     props: {
-      products,
+      products: optProducts,
     },
     revalidate: parseInt(process.env.REVALIDATE_SECONDS),
   };
 }
 
 export default function Home({ products }) {
-  console.log("[Home] - products ", products);
+  // console.log("[Home] - products ", products);
   return (
     <>
       <Head>
@@ -23,7 +27,7 @@ export default function Home({ products }) {
       </Head>
       <main className="p-3">
         <Title>Next Shop</Title>
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[repeat(3,20rem)] gap-8 justify-center">
           {products.map((product) => (
             <ProductCard product={product} key={product.id} />
           ))}
