b2d4a71dd20b5b6ae252b3386d27a1b188f25427
Sultan Ahmad Khan <saksaikot@gmail.com>
2022-02-18 21:42:44 +0600

    updated cms url to heroku

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   images: {
     deviceSizes: [320, 600, 750, 1080],
-    domains: ["localhost", "127.0.0.1"],
+    domains: ["localhost", "127.0.0.1", "next-js-blog-backend.herokuapp.com"],
   },
 };
