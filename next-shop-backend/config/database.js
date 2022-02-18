a0e9d50fe74dda026f4a35a9483068df812543d5
Sultan Ahmad Khan <saksaikot@gmail.com>
2022-02-18 14:02:30 +0600

    backend-heroku-test-1

--- next-shop-backend/config/database.js
+++ next-shop-backend/config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
   connection: {
     client: "sqlite",
     connection: {
-      filename: ".data/database.sqlite3",
-      // filename: env("DATABASE_FILENAME", ".data/database.sqlite3"),
+      filename: env("DATABASE_FILENAME", ".data/database.sqlite3"),
     },
     useNullAsDefault: true,
+    debug: false,
   },
 });
