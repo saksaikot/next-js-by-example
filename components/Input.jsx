30549fd1b320b08f54895d18d3a4ccaac10ce714
Sultan Ahmad Khan <saksaikot@gmail.com>
2022-02-19 15:50:25 +0600

    004 Form State and Validation

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
-export default function Input({ type }) {
-  return <input className="border rounded px-2 py-1 w-80" type={type} />;
-}
+const Input = forwardRef((props, ref) => {
+  return (
+    <input className="border rounded px-2 py-1 w-80" {...props} ref={ref} />
+  );
+});
+
+export default Input;
