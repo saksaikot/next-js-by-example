19e1238c4d43a3c683989be851de0683655f3395
Sultan Ahmad Khan <saksaikot@gmail.com>
2022-02-19 22:56:43 +0600

    009. Setting a Cookie

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,7 @@
 import { fetchJson } from "../../lib/api";
+import cookie from "cookie";
+const CMS_URL = process.env.CMS_URL;
+const WEEK_IN_SECONDS = 604800;
 
 export default async function loginHandler(req, res) {
   if (req.method !== "POST") {
@@ -7,7 +10,7 @@ export default async function loginHandler(req, res) {
   }
   const { email, password } = req.body;
   try {
-    const response = await fetchJson(`http://127.0.0.1:1337/api/auth/local`, {
+    const response = await fetchJson(`${CMS_URL}/api/auth/local`, {
       method: "POST",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify({ identifier: email, password }),
@@ -18,7 +21,17 @@ export default async function loginHandler(req, res) {
       user: { id, username },
     } = response;
     console.log("[loginHandler]", response);
-    return res.status(200).json({ id, name: username });
+    return res
+      .status(200)
+      .setHeader(
+        "Set-Cookie",
+        cookie.serialize("jwt", jwt, {
+          path: "/api",
+          httpOnly: true,
+          maxAge: WEEK_IN_SECONDS,
+        })
+      )
+      .json({ id, name: username });
   } catch (error) {
     // console.log(error);
     return res.status(error.status).end();
