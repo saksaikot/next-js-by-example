6937f086cfcc1f398e8f9f7631c103e7f09e7822
Sultan Ahmad Khan <saksaikot@gmail.com>
2022-02-19 18:46:31 +0600

    005. Sign-in API request

--- pages/sign-in.jsx
+++ pages/sign-in.jsx
@@ -3,16 +3,21 @@ import Button from "../components/Button.jsx";
 import Input from "../components/Input";
 import Label from "../components/Label";
 import Page from "../components/Page";
+import { fetchJson } from "../lib/api.js";
 
 export default function Signin() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
-  const handleOnsubmit = (event) => {
+  const handleOnsubmit = async (event) => {
     event.preventDefault();
-    const email = emailRef.current.value;
+    const identifier = emailRef.current.value;
     const password = passwordRef.current.value;
-    const data = { email, password };
-    console.log("[form submitted]", data);
+    const response = await fetchJson(`http://127.0.0.1:1337/api/auth/local`, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ identifier, password }),
+    });
+    console.log("signin response", response);
   };
   return (
     <Page title="Sign in">
