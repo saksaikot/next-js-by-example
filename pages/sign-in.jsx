375333ed57e72133346e3a9c1544feabea3a9dea
Sultan Ahmad Khan <saksaikot@gmail.com>
2022-02-19 20:14:25 +0600

    006. Error and Loading states

--- pages/sign-in.jsx
+++ pages/sign-in.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Button from "../components/Button.jsx";
 import Input from "../components/Input";
 import Label from "../components/Label";
@@ -6,19 +6,28 @@ import Page from "../components/Page";
 import { fetchJson } from "../lib/api.js";
 
 export default function Signin() {
+  const [status, setStatus] = useState({ loading: false, error: false });
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const handleOnsubmit = async (event) => {
     event.preventDefault();
+    setStatus({ loading: true, error: false });
     const identifier = emailRef.current.value;
     const password = passwordRef.current.value;
-    const response = await fetchJson(`http://127.0.0.1:1337/api/auth/local`, {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({ identifier, password }),
-    });
-    console.log("signin response", response);
+    try {
+      const response = await fetchJson(`http://127.0.0.1:1337/api/auth/local`, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ identifier, password }),
+      });
+      setStatus({ loading: false, error: false });
+
+      console.log("signin response", response);
+    } catch (error) {
+      setStatus({ loading: false, error: true });
+    }
   };
+  const { loading, error } = status;
   return (
     <Page title="Sign in">
       <form onSubmit={handleOnsubmit}>
@@ -28,7 +37,11 @@ export default function Signin() {
         <Label label="Password">
           <Input type="Password" ref={passwordRef} required />
         </Label>
-        <Button type="submit">Submit</Button>
+        {error && <p className="text-red-500">Invalid credential!!!</p>}
+
+        <Button type="submit" disabled={!loading}>
+          Submit
+        </Button>
       </form>
     </Page>
   );
