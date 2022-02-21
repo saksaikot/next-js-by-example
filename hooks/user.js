b096ddae00b25a48f250f94e3a302203e71fbd4b
Sultan Ahmad Khan <saksaikot@gmail.com>
2022-02-21 17:52:45 +0600

    007.useSignOut custom hook

--- hooks/user.js
+++ hooks/user.js
@@ -1,10 +1,50 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { fetchJson } from "../lib/api";
 const FIVE_MINUTES = 60 * 5 * 100;
+const USER_QUERY_KEY = "user";
+
+export function useSignIn() {
+  const queryClient = useQueryClient();
+
+  const signinMutation = useMutation(async ({ email, password }) => {
+    const user = await fetchJson(`/api/login`, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    return user;
+  });
+  return {
+    signInError: signinMutation.isError,
+    signInLoading: signinMutation.isLoading,
+    signIn: async ({ email, password }) => {
+      try {
+        const user = await signinMutation.mutateAsync({ email, password });
+        queryClient.setQueryData(USER_QUERY_KEY, user);
+        return user;
+      } catch (error) {
+        return undefined;
+      }
+    },
+  };
+}
+
+export function useSignOut() {
+  const queryClient = useQueryClient();
+  const mutation = useMutation(() => fetchJson("/api/logout"));
+  return {
+    signOut: async () => {
+      try {
+        await mutation.mutateAsync();
+        queryClient.setQueryData(USER_QUERY_KEY, undefined);
+      } catch (error) {}
+    },
+  };
+}
 
 export function useUser() {
   const { data: user } = useQuery(
-    "user",
+    USER_QUERY_KEY,
     async () => {
       try {
         const response = await fetchJson("/api/user");
--- pages/sign-in.jsx
+++ pages/sign-in.jsx
@@ -1,41 +1,29 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import Button from "../components/Button.jsx";
 import Input from "../components/Input";
 import Label from "../components/Label";
 import Page from "../components/Page";
-import { fetchJson } from "../lib/api.js";
+// import { fetchJson } from "../lib/api.js";
 import { useRouter } from "next/router";
-import { useMutation, useQueryClient } from "react-query";
+// import { useMutation, useQueryClient } from "react-query";
+import { useSignIn } from "../hooks/user.js";
 
 export default function Signin() {
+  const { signIn, signInError: error, signInLoading: loading } = useSignIn();
+
   const router = useRouter();
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
-  const queryClient = useQueryClient();
 
-  const signinMutation = useMutation(async () => {
-    const email = emailRef.current.value;
-    const password = passwordRef.current.value;
-    const user = await fetchJson(`/api/login`, {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
-    return user;
-  });
   const handleOnsubmit = async (event) => {
     event.preventDefault();
-    try {
-      const user = await signinMutation.mutateAsync();
-      console.log(user);
-      queryClient.setQueryData("user", user);
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const user = await signIn({ email, password });
+    if (user) {
       router.push("/");
-      console.log("signin response", user);
-    } catch (error) {
-      //isError in signinMutation
     }
   };
-  const { isloading: loading, isError: error } = signinMutation;
   return (
     <Page title="Sign in">
       <form onSubmit={handleOnsubmit}>
